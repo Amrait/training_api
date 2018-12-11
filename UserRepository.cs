25f72604e1cc5bed975d4f8603d05a172e091858
Oleksii Kozak <aleks.kozak.official@gmail.com>
2018-12-11 23:02:34 +0200

    Fix update

--- UserRepository.cs
+++ UserRepository.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TrainingApi.Entities;
 
 namespace TrainingApi
@@ -14,18 +12,50 @@ namespace TrainingApi
         {
             return _users.Values.ToList();
         }
-        //TODO GetById
+
+        public User GetById(Guid id)
+        {
+            User result = null;
+            // Just to throw some logging around.
+            if (!_users.TryGetValue(id, out result))
+            {
+                Console.WriteLine($"WARN: failed to find user with specified id {id.ToString()} in our records!");
+            }
+            return result;
+        }
+
         public void Insert(User user)
         {
             _users.Add(user._id, user);
         }
-        //TODO Update
-        public void Delete(Guid id)
+
+        public bool Update(Guid id, string userName, string password)
+        {
+            if (_users.ContainsKey(id))
+            {
+                User user = _users[id];
+                if (!String.IsNullOrWhiteSpace(userName))
+                {
+                    user._userName = userName;
+                }
+                if (!String.IsNullOrWhiteSpace(password))
+                {
+                    user._password = password;
+                }
+                _users[id] = user;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Delete(Guid id)
         {
             if (_users.ContainsKey(id)) // Returns true.
             {
                 _users.Remove(id); // This is the value at cat.
+                return true;
             }
+            return false;
         }
     }
 }
